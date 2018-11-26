b2ad2343eace9f73249d5af65edcbb82966c7d12
wuxiz <wuxizhazhahui@example.com>
2018-11-26 15:35:07 +0800

    玩了一天的css- -！

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users.html', function (req, res) {
+  res.render('users')
+})
+
+router.get('/brand.html', function (req, res) {
+  res.render('brand')
+})
+
+router.get('/phone.html', function (req, res) {
+  res.render('phone')
+})
 module.exports = router;
